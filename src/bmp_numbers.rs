e11537f9b962cd7c2aa31a23c88cd5c2bda540e2
aharbin5 <aharbin5@students.kennesaw.edu>
2023-07-26 13:19:26 -0400

    Update Graph v2
    Finish bmp_numbers basic function
    Put data lines in graph

--- src/bmp_numbers.rs
+++ src/bmp_numbers.rs
@@ -1,6 +1,10 @@
 use bmp::{Image, Pixel};
 use bmp::px as px;
 
+// This is probably the least efficient code I've ever written
+// These could be done with line functions in a maximum of 7 lines
+// I'll get around to that later, this works for now
+
 pub fn draw_number(number: u32, mut img: Image, x_pos: u32, y_pos: u32) -> Image
 {
     match number
@@ -21,15 +25,135 @@ pub fn draw_number(number: u32, mut img: Image, x_pos: u32, y_pos: u32) -> Image
 
             img
         },
-        /*1 => {},
-        2 => {},
-        3 => {},
-        4 => {},
-        5 => {},
-        6 => {},
-        7 => {},
-        8 => {},
-        9 => {},*/
+        1 => {
+            img.set_pixel(x_pos+1, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos, y_pos+1, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos+1, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos+3, px!(255,255,255));
+            img.set_pixel(x_pos, y_pos+4, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos+4, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos+4, px!(255,255,255));
+
+            img
+        },
+        2 => {
+            img.set_pixel(x_pos, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos+3, y_pos+1, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos, y_pos+3, px!(255,255,255));
+            img.set_pixel(x_pos, y_pos+4, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos+4, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos+4, px!(255,255,255));
+            img.set_pixel(x_pos+3, y_pos+4, px!(255,255,255));
+
+            img
+        },
+        3 => {
+            img.set_pixel(x_pos, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos+3, y_pos+1, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos+3, y_pos+3, px!(255,255,255));
+            img.set_pixel(x_pos, y_pos+4, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos+4, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos+4, px!(255,255,255));
+
+            img
+        },
+        4 => {
+            img.set_pixel(x_pos, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos+3, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos, y_pos+1, px!(255,255,255));
+            img.set_pixel(x_pos+3, y_pos+1, px!(255,255,255));
+            img.set_pixel(x_pos, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos+3, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos+3, y_pos+3, px!(255,255,255));
+            img.set_pixel(x_pos+3, y_pos+4, px!(255,255,255));
+
+            img
+        },
+        5 => {
+            img.set_pixel(x_pos+3, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos, y_pos+1, px!(255,255,255));
+            img.set_pixel(x_pos, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos+3, y_pos+3, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos+4, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos+4, px!(255,255,255));
+            img.set_pixel(x_pos, y_pos+4, px!(255,255,255));
+
+            img
+        },
+        6 => {
+            img.set_pixel(x_pos+2, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos, y_pos+1, px!(255,255,255));
+            img.set_pixel(x_pos, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos+3, y_pos+3, px!(255,255,255));
+            img.set_pixel(x_pos, y_pos+3, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos+4, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos+4, px!(255,255,255));
+
+            img
+        },
+        7 => {
+            img.set_pixel(x_pos, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos+3, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos+3, y_pos+1, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos+3, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos+4, px!(255,255,255));
+
+            img
+        },
+        8 => {
+            img.set_pixel(x_pos+1, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos, px!(255,255,255));
+
+            img.set_pixel(x_pos, y_pos+1, px!(255,255,255));
+            img.set_pixel(x_pos+3, y_pos+1, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos, y_pos+3, px!(255,255,255));
+            img.set_pixel(x_pos+3, y_pos+3, px!(255,255,255));
+
+            img.set_pixel(x_pos+1, y_pos+4, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos+4, px!(255,255,255));
+
+            img
+        },
+        9 => {
+            img.set_pixel(x_pos+1, y_pos, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos, px!(255,255,255));
+
+            img.set_pixel(x_pos, y_pos+1, px!(255,255,255));
+            img.set_pixel(x_pos+3, y_pos+1, px!(255,255,255));
+            img.set_pixel(x_pos+1, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos+3, y_pos+2, px!(255,255,255));
+            img.set_pixel(x_pos+3, y_pos+3, px!(255,255,255));
+
+            img.set_pixel(x_pos+1, y_pos+4, px!(255,255,255));
+            img.set_pixel(x_pos+2, y_pos+4, px!(255,255,255));
+
+            img
+        },
         1_u32..=u32::MAX => todo!(),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ mod config;
 mod bmp_numbers;
 
 /*
+
+Reference links
+
 https://docs.rs/reqwest/latest/reqwest/blocking/struct.RequestBuilder.html#method.headers
 https://docs.rs/reqwest/latest/reqwest/blocking/struct.Response.html
 https://www.weather.gov/documentation/services-web-api
@@ -16,6 +19,8 @@ https://docs.rs/serde_json/latest/serde_json/index.html
 https://docs.rs/serde_json/latest/serde_json/fn.from_value.html
 https://docs.rs/bmp/latest/bmp/
 https://docs.rs/bmp/latest/bmp/struct.Image.html
+
+Print_hourly format
 
 resp["properties"]["periods"][item]["startTime"]
 resp["properties"]["periods"][item]["endTime"]
@@ -30,13 +35,13 @@ Start: {} {} -> End: {} {}
 Rain %: {}
 Humidity: {}
 Temperature: {} {}
+
 */
 
 fn main() {
     let client = Client::new();
     let mut my_header_map = HeaderMap::new();
     my_header_map.insert(USER_AGENT, HeaderValue::from_static(config::return_user_agent()));
-    /*
     let resp = client.get(config::return_target_url())
                      .headers(my_header_map)
                      .send();
@@ -46,16 +51,14 @@ fn main() {
         Err(e) => println!("{}", e),
         Ok(t) => {
             let resp: Value = serde_json::from_str(&t.text().unwrap()).unwrap();
-            gen_hum_and_rain_graph(resp.clone());
+            draw_detailed_graph(resp.clone());
+            //gen_hum_and_rain_graph(resp.clone());
             //print_hourly(resp.clone());
         },
     }
-    */
-
-    draw_image_template();
 }
 
-fn draw_image_template()
+fn draw_detailed_graph(dataset: Value)
 {
     let mut img = Image::new(644, 120); // Width, Height
 
@@ -88,7 +91,6 @@ fn draw_image_template()
     }
 
     // Draws 0-156 hour reference
-    // TODO: Implement pulling in the time data from the API response to print correct time codes
     color = true;
     for x in 0..156
     {
@@ -97,7 +99,7 @@ fn draw_image_template()
             // 4x + 10
             for offset in 0..4
             {
-                img.set_pixel(4 * x + 10 + offset, 111, px!(255,255,255));
+                img.set_pixel(4 * x + 10 + offset, 112, px!(255,255,255));
             }
             color = false;
         }
@@ -107,11 +109,30 @@ fn draw_image_template()
         }
     }
 
+    // TEMPLATE DRAWING ENDS
+
+    for x in 0..156 {
+        // Draws humidity line
+        let y = 110 - serde_json::from_value::<u32>(dataset["properties"]["periods"][x]["relativeHumidity"]["value"].clone()).unwrap();
+        for offset in 0..4
+        {
+            img.set_pixel((4 * x + 10 + offset).try_into().unwrap(), y, px!(255, 125, 0));
+        }
+
+        // Draws rain line
+        let y = 110 - serde_json::from_value::<u32>(dataset["properties"]["periods"][x]["probabilityOfPrecipitation"]["value"].clone()).unwrap();
+        for offset in 0..4
+        {
+            img.set_pixel((4 * x + 10 + offset).try_into().unwrap(), y, px!(0, 125, 255));
+        }
+    }
+
+    // Draw hour markers every 4 hours
     for x in 0..39
     {
         // 16x + 9
-        img = bmp_numbers::draw_number(0, img.clone(), 16 * x + 10, 112);
-        img = bmp_numbers::draw_number(0, img.clone(), 16 * x + 14, 112);
+        img = bmp_numbers::draw_number(dataset["properties"]["periods"][x*4]["startTime"].as_str().unwrap()[11..12].parse::<u32>().unwrap(), img.clone(), (16 * x + 10).try_into().unwrap(), 113);
+        img = bmp_numbers::draw_number(dataset["properties"]["periods"][x*4]["startTime"].as_str().unwrap()[12..13].parse::<u32>().unwrap(), img.clone(), (16 * x + 14).try_into().unwrap(), 113);
     }
 
     let _ = img.save("template_test.bmp");
